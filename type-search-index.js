d051a8dd7453f643e04eb3290e265f8455d93c56
TheTechdoodle <admin+github@darkender.com>
2020-04-28 00:14:38 -0700

    Publishing javadoc for regionapi:1.0-SNAPSHOT

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.darkender.plugins.regionapi","l":"NBTPositionReplace"},{"p":"com.darkender.plugins.regionapi","l":"Region"},{"p":"com.darkender.plugins.regionapi","l":"RegionAPI"}]
